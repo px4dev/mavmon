212e2dfc80e5a0fb34509c3386164795ad74962b
pxdev <px4@purgatory.org>
2012-12-06 23:23:17 -0800

    Add some printf debugging support. Printf is Very Big.

--- board.h
+++ board.h
@@ -1,4 +1,5 @@
 #include <u8g.h>
+#include <stdio.h>
 
 #ifdef __cplusplus
 # define EXTERN extern "C"
@@ -6,8 +7,11 @@
 # define EXTERN extern
 #endif
 
+#define debug(fmt, args...)	do { printf(fmt "\r\n", ##args); } while(0)
+
 EXTERN void	board_setup(void);
 EXTERN void	led_toggle(void);
+EXTERN void	led_on(void);
 EXTERN void	serial_start(unsigned bitrate);
 EXTERN void	serial_stop(void);
 
