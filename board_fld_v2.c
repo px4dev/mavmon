6ef9831f1d7afe80ba8941442f38fda59d239dbb
pxdev <px4@purgatory.org>
2012-12-06 23:25:52 -0800

    Fix the display initialisation sequence and display layout. Increase task stack sizes. Add a more complex display test. Hook up the u8g delay function. Now, we get pixels.

--- board_fld_v2.c
+++ board_fld_v2.c
@@ -3,6 +3,9 @@
 #include <libopencm3/stm32/usart.h>
 #include <libopencm3/stm32/spi.h>
 
+#include <stdio.h>
+#include <errno.h>
+
 #include "board.h"
 
 void
@@ -45,8 +48,8 @@ board_setup(void)
 	gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, GPIO_USART1_RX);
 
 	/* configure SPI GPIOs */
-	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_SPI1_SCK);
-	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_SPI1_MOSI);
+	gpio_set_mode(GPIO_BANK_SPI1_SCK,  GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_SPI1_SCK);
+	gpio_set_mode(GPIO_BANK_SPI1_MOSI, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_SPI1_MOSI);
 
 	/* configure display select/control GPIOs */
 	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO2);	/* /RST */
@@ -59,6 +62,12 @@ void
 led_toggle()
 {
 	gpio_toggle(GPIOA, GPIO11);
+}
+
+void
+led_on()
+{
+	gpio_set(GPIOA, GPIO11);	
 }
 
 void
@@ -108,6 +117,21 @@ usart1_isr(void)
 	}
 }
 
+int 
+_write(int file, char *ptr, int len)
+{
+        int i;
+
+        if (file == 1) {
+                for (i = 0; i < len; i++)
+                        usart_send_blocking(USART1, ptr[i]);
+                return i;
+        }
+
+        errno = EIO;
+        return -1;
+}
+
 /*
  * u8g interface drivers
  */
@@ -118,6 +142,8 @@ u8g_board_com_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr)
 	switch(msg)
 	{
 	case U8G_COM_MSG_INIT:
+		//debug("u8com: init");
+
 		/* configure SPI */
 		/*
 		 * SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
@@ -133,7 +159,7 @@ u8g_board_com_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr)
 		 */
 		spi_init_master(
 			SPI1, 
-			SPI_CR1_BAUDRATE_FPCLK_DIV_64,	/* XXX need to scope this - maybe able to go to 2 */
+			SPI_CR1_BAUDRATE_FPCLK_DIV_2,	/* XXX need to scope this - maybe able to go to 2 */
 			SPI_CR1_CPOL_CLK_TO_1_WHEN_IDLE,
 			SPI_CR1_CPHA_CLK_TRANSITION_1,	/* want latch-on-rising */
 			SPI_CR1_DFF_8BIT,
@@ -142,6 +168,7 @@ u8g_board_com_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr)
 		spi_enable_ss_output(SPI1);
 		spi_set_nss_high(SPI1);
 		spi_enable(SPI1);
+
 		break;
 
 	case U8G_COM_MSG_STOP:
@@ -149,9 +176,11 @@ u8g_board_com_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr)
 
 	case U8G_COM_MSG_ADDRESS:
 		if (arg_val) {
+			//debug("u8com: data");
 			/* display data */
 			gpio_set(GPIOA, GPIO3);
 		} else {
+			//debug("u8com: command");
 			/* display command */
 			gpio_clear(GPIOA, GPIO3);
 		}
@@ -159,9 +188,11 @@ u8g_board_com_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr)
 
 	case U8G_COM_MSG_CHIP_SELECT:
 		if (arg_val) {
+			//debug("u8com: select");
 			/* select display */
 			gpio_clear(GPIOA, GPIO4);
 		} else {
+			//debug("u8com: deselect");
 			/* deselect display */
 			gpio_set(GPIOA, GPIO4);
 		}
@@ -169,25 +200,31 @@ u8g_board_com_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr)
 
 	case U8G_COM_MSG_RESET:
 		if (arg_val) {
+			//debug("u8com: clear reset");
 			/* set reset line high */
 			gpio_set(GPIOA, GPIO2);
 		} else {
+			//debug("u8com: assert reset");
 			/* set reset line low */
 			gpio_clear(GPIOA, GPIO2);
 		}
 		break;
 
 	case U8G_COM_MSG_WRITE_BYTE:
-		spi_send(SPI1, arg_val);
+		//debug("u8com: send 0x%02x", arg_val);
+		spi_xfer(SPI1, arg_val);
 		break;
 
 	case U8G_COM_MSG_WRITE_SEQ:
 	case U8G_COM_MSG_WRITE_SEQ_P:
+		//debug("u8com: seq %d", arg_val);
 		{
 			uint8_t *ptr = (uint8_t *)arg_ptr;
 
-			while (arg_val-- > 0)
-				spi_send(SPI1, *ptr++);
+			while (arg_val-- > 0) {
+				//debug("u8com:   0x%02x", *ptr);
+				spi_xfer(SPI1, *ptr++);
+			}
 			/* XXX wait for the last byte to finish transferring... */
 		}
 		break;
@@ -200,28 +237,27 @@ u8g_board_com_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr)
 #define PAGE_HEIGHT 8
 
 static const uint8_t u8g_dev_init_seq[] = {
-	U8G_ESC_CS(0),		/* disable chip */
+	U8G_ESC_CS(0),		/* de-select display */
 	U8G_ESC_ADR(0),		/* instruction mode */
 	U8G_ESC_RST(1),		/* do reset low pulse with (1*16)+2 milliseconds */
-	U8G_ESC_CS(1),		/* enable chip */
-	
-	0xab,			/* new  */
+	U8G_ESC_DLY(1),		/* delay 1 ms after releasing reset */
+	U8G_ESC_CS(1),		/* select display */
+
 	0xaf,			/* display on */
-	0x40,			/* display start line=0 */
-	0xc0,			/* Common output mode select= reverse   */
+	0x40,			/* display start line = 0 */
+	0xc0,			/* COM scan direction (normal) */
 	0xa6,			/* normal display */
-	0xa4,			/* Duisplay all point = off */
-	0xa3,			/* LCD bias = 1/9   a2 */
+	0xa1,
+	0xa4,			/* clear display */
+	0xa2,			/* LCD bias = 1/9 */
 	0x2f,			/* Power control = all on */
-	0x25,			/* Rab Ratio     26 */
-	0x81, 0x17,		/* E-Vol setting */
+	0x23,			/* Rab Ratio  */
+	0x81, 0x25,		/* E-Vol setting */
 
-
 	U8G_ESC_DLY(100),	/* delay 100 ms */
-	0x0a5,			/* display all points, ST7565 */
+	0xa5,			/* all-pixels-on */
 	U8G_ESC_DLY(100),	/* delay 100 ms */
-	U8G_ESC_DLY(100),	/* delay 100 ms */
-	0x0a4,			/* normal display */
+	0xa4,			/* all-pixels-off */
 	U8G_ESC_CS(0),		/* disable chip */
 	U8G_ESC_END		/* end of sequence */
 };
@@ -229,8 +265,8 @@ static const uint8_t u8g_dev_init_seq[] = {
 static const uint8_t u8g_dev_data_start[] = {
 	U8G_ESC_ADR(0),		/* instruction mode */
 	U8G_ESC_CS(1),		/* enable chip */
-	0x010,			/* set upper 4 bit of the col adr to 0 */
-	0x000,			/* set lower 4 bit of the col adr to 0 */      
+	0x10,			/* set upper 4 bit of the col adr to 0 */
+	0x00,			/* set lower 4 bit of the col adr to 0 */      
 	U8G_ESC_END		/* end of sequence */
 };
 
@@ -239,6 +275,7 @@ uint8_t u8g_board_dev_fn(u8g_t *u8g, u8g_dev_t *dev, uint8_t msg, void *arg)
 	switch(msg)
 	{
 	case U8G_DEV_MSG_INIT:
+		//debug("u8dev: init");
 		u8g_InitCom(u8g, dev);
 		u8g_WriteEscSeqP(u8g, dev, u8g_dev_init_seq);
 		break;
@@ -249,19 +286,32 @@ uint8_t u8g_board_dev_fn(u8g_t *u8g, u8g_dev_t *dev, uint8_t msg, void *arg)
 	case U8G_DEV_MSG_PAGE_NEXT:
 		{
 			u8g_pb_t *pb = (u8g_pb_t *)(dev->dev_mem);
-			u8g_WriteEscSeqP(u8g, dev, u8g_dev_data_start);    
-			u8g_WriteByte(u8g, dev, 0x0b0 | pb->p.page);	/* select current page (ST7565R) */
+
+			u8g_SetAddress(u8g, dev, 0);				/* command mode */
+			u8g_SetChipSelect(u8g, dev, 1);				/* select display */
+			u8g_WriteByte(u8g, dev, 0xb0 + (pb->p.page & 0x7));	/* select current page */
+			u8g_WriteByte(u8g, dev, 0x00);				/* select column zero */
+			u8g_WriteByte(u8g, dev, 0x10);				/* select column zero */
+			u8g_WriteByte(u8g, dev, 0xaf);				/* display on */
+			u8g_SetAddress(u8g, dev, 1);				/* data mode */
+			u8g_pb_WriteBuffer(pb, u8g, dev);			/* write the page */
+			u8g_SetChipSelect(u8g, dev, 0);				/* deselect display */
+
+#if 0
+			u8g_WriteEscSeqP(u8g, dev, u8g_dev_data_start);
+			u8g_WriteByte(u8g, dev, 0xb0 | pb->p.page);	/* select current page (ST7565R) */
 			u8g_SetAddress(u8g, dev, 1);			/* data mode */
 			if (u8g_pb_WriteBuffer(pb, u8g, dev) == 0)
 			  	return 0;
 			u8g_SetChipSelect(u8g, dev, 0);
+#endif
 		}
 		break;
 
 	case U8G_DEV_MSG_CONTRAST:
 		u8g_SetChipSelect(u8g, dev, 1);
 		u8g_SetAddress(u8g, dev, 0);          /* instruction mode */
-		u8g_WriteByte(u8g, dev, 0x081);
+		u8g_WriteByte(u8g, dev, 0x81);
 		u8g_WriteByte(u8g, dev, (*(uint8_t *)arg) >> 2);
 		u8g_SetChipSelect(u8g, dev, 0);      
 		return 1;
@@ -270,14 +320,3 @@ uint8_t u8g_board_dev_fn(u8g_t *u8g, u8g_dev_t *dev, uint8_t msg, void *arg)
 }
 
 U8G_PB_DEV(u8g_board_dev, WIDTH, HEIGHT, PAGE_HEIGHT, u8g_board_dev_fn, u8g_board_com_fn);
-
-/*
- * u8g delay functions
- */
-void u8g_Delay(uint16_t val)
-{
-	/* do not know how to delay... */
-}
-void u8g_MicroDelay(void)
-{
-}
--- main.cpp
+++ main.cpp
@@ -4,13 +4,15 @@
 #include <u8g.h>
 #include <scmRTOS.h>
 
+#include <stdio.h>
+
 /* initialiser list */
 extern unsigned long __ctors_start__;
 extern unsigned long __ctors_end__;
 
 /* process list, must match scmRTOS_PROCESS_COUNT */
-typedef OS::process<OS::pr0, 300> TProc1;
-typedef OS::process<OS::pr1, 300> TProc2;
+typedef OS::process<OS::pr0, 1024> TProc1;
+typedef OS::process<OS::pr1, 1024> TProc2;
 
 TProc1 Proc1;
 TProc2 Proc2;
@@ -19,16 +21,85 @@ OS::TEventFlag TimerEvent;
 
 u8g_t u8g;
 
-static void draw(void)
-{
-	u8g_SetFont(&u8g, u8g_font_6x10);
-	u8g_DrawStr(&u8g, 0, 15, "Hello World!");
+void u8g_prepare(void) {
+  u8g_SetFont(&u8g, u8g_font_6x10);
+  u8g_SetFontRefHeightExtendedText(&u8g);
+  u8g_SetDefaultForegroundColor(&u8g);
+  u8g_SetFontPosTop(&u8g);
 }
 
-int main(void)
+void u8g_box_frame(uint8_t a) {
+  u8g_DrawStr(&u8g, 0, 0, "drawBox");
+  u8g_DrawBox(&u8g, 5,10,20,10);
+  u8g_DrawBox(&u8g, 10+a,15,30,7);
+  u8g_DrawStr(&u8g, 0, 30, "drawFrame");
+  u8g_DrawFrame(&u8g, 5,10+30,20,10);
+  u8g_DrawFrame(&u8g, 10+a,15+30,30,7);
+}
+
+void u8g_string(uint8_t a) {
+  u8g_DrawStr(&u8g, 30+a,31, " 0");
+  u8g_DrawStr90(&u8g, 30,31+a, " 90");
+  u8g_DrawStr180(&u8g, 30-a,31, " 180");
+  u8g_DrawStr270(&u8g, 30,31-a, " 270");
+}
+
+void u8g_line(uint8_t a) {
+  u8g_DrawStr(&u8g, 0, 0, "drawLine");
+  u8g_DrawLine(&u8g, 7+a, 10, 40, 55);
+  u8g_DrawLine(&u8g, 7+a*2, 10, 60, 55);
+  u8g_DrawLine(&u8g, 7+a*3, 10, 80, 55);
+  u8g_DrawLine(&u8g, 7+a*4, 10, 100, 55);
+}
+
+void u8g_ascii_1(void) {
+  char s[2] = " ";
+  uint8_t x, y;
+  u8g_DrawStr(&u8g, 0, 0, "ASCII page 1");
+  for( y = 0; y < 6; y++ ) {
+    for( x = 0; x < 16; x++ ) {
+      s[0] = y*16 + x + 32;
+      u8g_DrawStr(&u8g, x*7, y*10+10, s);
+    }
+  }
+}
+
+void u8g_ascii_2(void) {
+  char s[2] = " ";
+  uint8_t x, y;
+  u8g_DrawStr(&u8g, 0, 0, "ASCII page 2");
+  for( y = 0; y < 6; y++ ) {
+    for( x = 0; x < 16; x++ ) {
+      s[0] = y*16 + x + 160;
+      u8g_DrawStr(&u8g, x*7, y*10+10, s);
+    }
+  }
+}
+
+
+uint8_t draw_state = 0;
+
+void draw(void) {
+  u8g_prepare();
+  switch(draw_state >> 3) {
+    case 0: u8g_box_frame(draw_state&7); break;
+    case 1: u8g_string(draw_state&7); break;
+    case 2: u8g_line(draw_state&7); break;
+    case 3: u8g_ascii_1(); break;
+    case 4: u8g_ascii_2(); break;
+  }
+}
+
+
+int 
+main(void)
 {
 	/* configure the board */
 	board_setup();
+
+	/* XXX debugging */
+	serial_start(115200);
+	debug("mavmon");
 
 	/* run constructors */
 	for (unsigned long *ctors = &__ctors_start__; ctors < &__ctors_end__; )
@@ -45,23 +116,31 @@ namespace OS
 	{
 		unsigned count = 0;
 		for (;;) {
-			if (count++ > 250) {
+			if (count++ > 125) {
 				led_toggle();     /* LED on/off */
 				count = 0;
 			}
 			TimerEvent.wait();
 		}
 	}
+
 	template <>
 	OS_PROCESS void TProc2::exec()
 	{
 		u8g_Init(&u8g, &u8g_board_dev);
-
-		do {
-			draw();
-		} while (u8g_NextPage(&u8g));
+		u8g_SetRot180(&u8g);
+		u8g_SetColorIndex(&u8g, 1);		/* pixel on */
 
 		for (;;) {
+			u8g_FirstPage(&u8g);
+			do {
+				draw();
+			} while ( u8g_NextPage(&u8g) );
+			
+			draw_state++;
+			if ( draw_state >= 5*8 )
+				draw_state = 0;			
+
 			sleep(100);
 		}
 	}
@@ -73,3 +152,11 @@ void OS::system_timer_user_hook()
 	TimerEvent.signal_isr();
 }
 
+/*
+ * u8g delay functions
+ */
+extern "C" void
+u8g_Delay(uint16_t val)
+{
+	OS::sleep(val);
+}
